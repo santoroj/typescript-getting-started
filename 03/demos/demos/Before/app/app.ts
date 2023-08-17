88bea50c520b195f1b860af41332c0412fd9146f
Joes GUEST <Joe.Santoro@raytheon.co.uk>
2023-08-17 17:58:21 +0100

    Complete section 4 part 2

--- 03/demos/demos/Before/app/app.ts
+++ 03/demos/demos/Before/app/app.ts
@@ -1,7 +1,16 @@
 function startGame() {
   // starting a new game
-  var messagesElement = document.getElementById('messages');
+
+  let playerName: string = "Audry";
+  logPlayer(playerName);
+
+  
+  let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
