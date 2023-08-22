3afa0322d9a478e5d6fbad27b089efaa2874485c
Joes GUEST <Joe.Santoro@raytheon.co.uk>
2023-08-22 15:05:05 +0100

    Completed course

---
+++ 05/demos/demos/Before/js/app.js
@@ -0,0 +1,49 @@
+var Player = (function () {
+    function Player() {
+    }
+    Player.prototype.formatName = function () {
+        return this.name.toUpperCase();
+    };
+    return Player;
+}());
+function startGame() {
+    var playerName = getInputValue('playername');
+    logPlayer(playerName);
+    postScore(80, playerName);
+    postScore(-5, playerName);
+}
+function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
+    console.log("New game starting for player: ".concat(name));
+}
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = "".concat(score, " - ").concat(playerName);
+    logger("Score: ".concat(score));
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+function logError(err) {
+    console.error(err);
+}
+var firstPlayer = new Player();
+firstPlayer.name = 'Lanier';
+console.log(firstPlayer.formatName());
+//# sourceMappingURL=app.js.map
